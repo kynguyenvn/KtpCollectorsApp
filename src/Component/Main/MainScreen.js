f428c15b343f6dc81c3679eecdbaaed76c592bdd
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-25 23:05:51 +0700

    [Update] MainScreen

--- src/Component/Main/MainScreen.js
+++ src/Component/Main/MainScreen.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {View} from 'react-native';
+import R from '../R';
+import { TabBar } from '../Common/TabBar';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import { GoodUnitAndReject } from './GoodUnitAndReject';
+import { UnPlannedDowntime } from '../UPT/UnPlannedDowntime';
+import {PlannedDowntime} from '../PDT/PlannedDowntime';
+import {Changeover} from '../Changeover/Changeover';
 
 
 /**
@@ -19,12 +26,36 @@ export default class MainScreen extends Component {
     render(){
 
         return(
+            
+            <View style={{flex:1}}>
+            
+                <ScrollableTabView
+                    tabBarPosition='top'
+                    initialPage={0}
+                    locked
+                    prerenderingSiblingsNumber={3}
+                    renderTabBar={() => <TabBar />}
+                >
 
-            <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <GoodUnitAndReject
+                        tabLabel={''}
+                    />
 
-                <Text>Main Screen</Text>
+                    <UnPlannedDowntime
+                        tabLabel={'Unplanned D/T'.toUpperCase()}
+                    />
 
-            </SafeAreaView>
+                    <Changeover
+                        tabLabel={'Changeover'.toUpperCase()}
+                    />
+
+                    <PlannedDowntime
+                        tabLabel={'Planned D/T'.toUpperCase()}
+                    />
+
+                </ScrollableTabView>
+
+            </View>
 
         );
     }
