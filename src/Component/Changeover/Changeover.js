4a7c31adee68f1fa19a322f05a2b70cc1f3184c5
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-25 23:04:45 +0700

    [Update] small UI GoodUnitAndReject, PlannedDowntime, Changeover, UnPlannedDowntime

---
+++ src/Component/Changeover/Changeover.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+/**
+ * props:{}
+ * state:{}
+ */
+class Changeover extends Component {
+
+    render(){
+
+        return(
+
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#FFF', paddingHorizontal:50}}>
+
+                <Text>Changeover</Text>
+
+            </View>
+        );
+    }
+}
+
+export {Changeover}
---
+++ src/Component/PDT/PlannedDowntime.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+/**
+ * props:{}
+ * state:{}
+ */
+class PlannedDowntime extends Component {
+
+    render(){
+
+        return(
+
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#FFF', paddingHorizontal:50}}>
+
+                <Text>PlannedDowntime</Text>
+
+            </View>
+        );
+    }
+}
+
+export {PlannedDowntime}
