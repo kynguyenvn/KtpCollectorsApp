4d2f7c951b2c03c907f3ca74293e9e82cb1b6dea
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 20:33:48 +0700

    [Update] Component General

--- src/Component/Common/Selector.js
+++ src/Component/Common/Selector.js
@@ -1,5 +1,58 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
+import R from '../R';
+
+/**
+ * props:{}
+ */
+const CGeneral = (props) => {
+
+    return  <View style={{flex: 1, flexDirection:'column', backgroundColor:'#FFF', paddingHorizontal:50, paddingBottom:30}}>
+        {props.children}
+    </View>
+};
+
+
+/**
+ * @param props
+ * @param title
+ */
+const CDetails = (props) => {
+
+    const {goBack=()=>{}, title=''} = props;
+
+    return <View style={{flex:1, backgroundColor:'#F3F5F7', borderWidth:1, borderColor:'#CAD3DB', borderRadius:3}}>
+
+        <View style={{flex:123, flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+            
+            <Text style={{flex:1, fontSize:30, color:'#3F4246', fontWeight:'600', lineHeight:40, textAlign:'center', marginLeft:71}}>{title.toUpperCase()}</Text>
+            
+            <TouchableOpacity activeOpacity={0.8} onPress={goBack} style={{height:40, width:40, borderRadius:20, justifyContent:'center', alignItems:'center', backgroundColor:'#FFF', marginRight:30, borderWidth:1, borderColor:'#DBE2E8', ...R.Style.shadowIcon}}>
+                
+                <Text>X</Text>
+            
+            </TouchableOpacity>
+
+        </View>
+
+        <View style={{flex:364, flexDirection:'row'}}>
+            {props.children}
+        </View>
+
+    </View>
+};
+
+/**
+ * 
+ */
+const CSmallBox = (props) => {
+
+    const {isBorderLeftWidth=false} = props;
+
+    return <View style={{flex:1, borderTopWidth:1, borderLeftWidth:isBorderLeftWidth?1:null, borderColor:'#CAD3DB'}}>
+        {props.children}
+    </View>
+};
 
 
 /**
@@ -30,5 +83,8 @@ const CButton = ({buttonName='', onPress=()=>{}, marginTop=30, color='#FFF', bac
 
 
 export {
-    CButton
+    CButton,
+    CGeneral,
+    CDetails,
+    CSmallBox
 }
