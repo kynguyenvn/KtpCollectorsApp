cb1ec88d68ea2146a1a73142087281eb11251c45
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 20:34:08 +0700

    [Update] shadowButton for UStyle

--- src/System/UStyle.js
+++ src/System/UStyle.js
@@ -4,5 +4,6 @@ export default class UStyle {
 
     static shadow = {elevation: 5, shadowOffset: {width: 0, height: 4}, shadowRadius:16, shadowColor: '#000000', shadowOpacity: 0.15};
     static shadowIcon = {elevation: 2, shadowOffset: {width: 0, height: 2}, shadowRadius:2, shadowColor: '#000000', shadowOpacity: 0.15};
+    static shadowButton = {elevation: 2, shadowOffset: {width: 0, height: 4}, shadowRadius:8, shadowColor: '#296C42', shadowOpacity: 0.2};
 
 }   
