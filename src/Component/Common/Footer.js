09e1d4f0ee81065be24a521dbbf0a6d7e743be31
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 14:18:04 +0700

    [Update] UI Footer

---
+++ src/Component/Common/Footer.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import moment from 'moment';
+
+/**
+ * props:{}
+ * state:{
+ *          date: moment().format('DD MMM, YYYY - h:mm:ss')
+ * }
+ */
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+            date: moment().format('DD MMM, YYYY - h:mm:ss')
+        }
+    }
+
+    /**
+     * 
+     */
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                date: moment().format('DD MMM, YYYY - h:mm:ss')
+            })
+        }, 1000)
+    }
+
+    render(){
+
+        const {date} = this.state;
+
+        return <View style={{height:60, flexDirection:'row', alignItems:'center', justifyContent:'space-between', backgroundColor:'#F3F5F7', paddingHorizontal:50}}>
+
+            <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20}}>Powered by Kyzentree &#64;2020</Text>
+
+            <Text style={{fontSize:16, color:'#3F4246', fontWeight:'600', lineHeight:20}}>{date} - Changeover</Text>
+
+            <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20}}>v2.0.1</Text>
+
+        </View>
+    }
+};
+
+export {Footer}
