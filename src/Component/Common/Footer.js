94695bd7610a5af6ae6d28e81d9b42668b76f6a9
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 20:33:20 +0700

    [Update] Footer

--- src/Component/Common/Footer.js
+++ src/Component/Common/Footer.js
@@ -36,12 +36,16 @@ class Footer extends Component {
 
         return <View style={{height:60, flexDirection:'row', alignItems:'center', justifyContent:'space-between', backgroundColor:'#F3F5F7', paddingHorizontal:50}}>
 
-            <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20}}>Powered by Kyzentree &#64;2020</Text>
+            <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20, flex:1}}>Powered by Kyzentree &#64;2020</Text>
 
-            <Text style={{fontSize:16, color:'#3F4246', fontWeight:'600', lineHeight:20}}>{date} - Changeover</Text>
-
-            <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20}}>v2.0.1</Text>
-
+            <Text style={{fontSize:16, color:'#3F4246', fontWeight:'600', lineHeight:20, flex:1, marginLeft:16}}>{date} - Changeover</Text>
+            
+            <View style={{flex:1, alignItems:'flex-end'}}>
+                
+                <Text style={{fontSize:14, color:'#6B7B8B', lineHeight:20}}>v2.0.1</Text>
+            
+            </View>
+        
         </View>
     }
 };
