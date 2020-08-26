03debbef5a97048ce450af1536517d490571da2c
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 14:18:31 +0700

    [Add] shadowIcon for UStyle

--- src/System/UStyle.js
+++ src/System/UStyle.js
@@ -3,5 +3,6 @@ import {} from 'react-native';
 export default class UStyle {
 
     static shadow = {elevation: 5, shadowOffset: {width: 0, height: 4}, shadowRadius:16, shadowColor: '#000000', shadowOpacity: 0.15};
+    static shadowIcon = {elevation: 2, shadowOffset: {width: 0, height: 2}, shadowRadius:2, shadowColor: '#000000', shadowOpacity: 0.15};
 
 }   
