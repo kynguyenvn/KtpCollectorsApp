08ce01b0bc6e663492692c9f8f56b66babd58a84
Nam Nguyen <atelier.c8de@gmail.com>
2020-08-26 14:18:42 +0700

    [Fix] TabBar

--- src/Component/Common/TabBar.js
+++ src/Component/Common/TabBar.js
@@ -1,43 +1,27 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, FlatList} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 
 /**
  * 
  */
-const TabBar = ({tabs, activeTab, goToPage}) => {
+const TabBar = ({tabs, activeTab, goToPage, style}) => {
 
-    return <View style={{backgroundColor:'#F3F5F7', flexDirection:'row', justifyContent:'space-between', paddingTop:10, paddingBottom:30, paddingHorizontal:50}}>
+    return <View style={{flexDirection:'row', backgroundColor: '#F3F5F7', paddingTop: 10, paddingBottom: 30, paddingHorizontal: 50, ...style}}>
+
+        {tabs.map((tab, i) => {
+            return i===0?null:<TouchableOpacity key={tab} activeOpacity={0.8} onPress={() => goToPage(i)} style={{flex: 1, height: 70, flexDirection: 'row', alignItems: 'center', justifyContent:'center', backgroundColor:activeTab===i?'#405CD7':'#00538B', marginLeft:i===1?null:20, borderRadius: 3, overflow:'hidden'}}>
+
+                <View style={{width: 20, height: 20, borderRadius: 10, marginRight: 15, backgroundColor: '#FFF'}} />
+
+                <Text numberOfLines={1} style={{fontSize: 20, color: '#FFFFFF', fontWeight: '600', lineHeight: 30}}>{tab}</Text>
+            
+            </TouchableOpacity>;
         
-        <FlatList
-            data={tabs.map(tab=>({label: tab}))}
-            keyExtractor={item => item.label}
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-
-            horizontal={true}
-
-            // ListHeaderComponent={()=><View style={{width: 30}}/>}
-            ItemSeparatorComponent={()=><View style={{width: 20}}/>}
-            // ListFooterComponent={()=><View style={{width: 30}}/>}
-
-            renderItem={
-                ({item, index}) =>{
-                    return <View style={{height: 70, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', backgroundColor: activeTab === index ?'#405CD7':'#00538B', paddingHorizontal:30, borderRadius: 3}}>
-
-                        <TouchableOpacity onPress={() => goToPage(index)} style={{flex: 1, flexDirection:'row', alignItems: 'center'}}>
-                            
-                            <View style={{width:20, height:20, borderRadius:10, marginRight:15, backgroundColor:'#FFF'}} />
-                            
-                            <Text style={{fontSize:20, color:'#FFFFFF', fontWeight:'600', lineHeight:30}}>{item.label}</Text>
-                        
-                        </TouchableOpacity>
-
-                    </View>;
-                }}
-        />
+        })}
+    
     </View>
 };
 
 
-export {TabBar}
+export { TabBar }
