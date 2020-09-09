8d973a43319ae32ae73829fbe781c88a3fb91d80
nguyen <kynguyen92bkdn@gmail.com>
2020-09-09 23:26:32 +0700

    add timer

--- src/Component/Main/MainScreen.js
+++ src/Component/Main/MainScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {TabBar} from '../Common/TabBar';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import moment from 'moment';
+import {TabBar} from '../Common/TabBar';
 import {GoodUnitAndReject} from './GoodUnitAndReject';
 import {UnPlannedDowntime} from '../UPT/UnPlannedDowntime';
 import {PlannedDowntime} from '../PDT/PlannedDowntime';
@@ -9,16 +10,15 @@ import {Changeover} from '../Changeover/Changeover';
 import {Footer} from '../Common/Footer';
 import {AlertScreen} from '../Common/AlertScreen';
 
-
 /**
  * mock data
  */
 const shift = {
-  start: 1598846400000, end: 1598889600000, shiftlyBuiltUnits: 3000,
-  completeHours: [
-  {builtUnits: 1000, hourStart: 1598857200000}
-  ]
-  }
+  start: 1598846400000,
+  end: 1598889600000,
+  shiftlyBuiltUnits: 3000,
+  completeHours: [{builtUnits: 1000, hourStart: 1598857200000}],
+};
 
 /**
  * props:{}
@@ -30,19 +30,66 @@ export default class MainScreen extends Component {
     super(props);
     this.state = {
       currentTime: new Date().getTime(),
-      minute: new Date().getMinutes()
-    }
+      minute: new Date().getMinutes(),
+      idleStart: Date.now(),
+      isActive: true,
+      isEverythingOk: false,
+    };
   }
-
 
   UNSAFE_componentWillMount() {
     this.startClockEvents();
   }
 
+  componentDidMount() {
+    this.wasActive = false;
+    if (this.state.isActive) {
+      this.wasActive = true;
+      this.activated();
+    }
+  }
   componentWillUnmount() {
+    this.disabled();
     clearTimeout(this.firstMinuteTimeout);
     clearInterval(this.minuteInterval);
   }
+
+  componentDidUpdate() {
+    if (this.state.isActive) {
+      if (!this.wasActive) {
+        this.wasActive = true;
+        this.activated();
+      }
+    } else if (this.wasActive) {
+      this.wasActive = false;
+      this.disabled();
+    }
+  }
+
+  activated = () => {
+    if (!this.timer) {
+      this.timer = setInterval(() => this.checkIdle(), 1000);
+      this.setState({idleStart: Date.now()});
+    }
+  };
+
+  disabled = () => {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+  };
+
+  checkIdle = () => {
+    const startTime = moment(this.state.idleStart);
+    const now = moment(Date.now());
+    const diff = now.diff(startTime, 'seconds');
+    const idleTimeout = 50;
+    if (!this.state.isEverythingOk && diff > idleTimeout) {
+      console.log('show everything ok');
+      this.setState({isEverythingOk: true});
+    }
+  };
 
   startClockEvents = () => {
     const secondInMillisecs = 1000;
@@ -50,21 +97,21 @@ export default class MainScreen extends Component {
     const minuteInMillisecs = minuteInSecs * secondInMillisecs;
     const currentTime = new Date();
 
-    this.setState({ currentTime: currentTime.getTime()})
+    this.setState({currentTime: currentTime.getTime()});
     this.firstMinuteTimeout = setTimeout(() => {
       this.minutePassed();
       this.minuteInterval = setInterval(
         () => this.minutePassed(),
-        minuteInMillisecs
+        minuteInMillisecs,
       );
     }, (minuteInSecs - currentTime.getSeconds()) * secondInMillisecs);
-  }
+  };
 
-  minutePassed = ()=> {
+  minutePassed = () => {
     let minuteMark = new Date().getMinutes();
     const currentTime = new Date().getTime();
     if (minuteMark === 59) {
-      console.log('oneHourPass')
+      console.log('oneHourPass');
     }
     if(minuteMark === 0){minuteMark =+ 1}
     this.setState({ currentTime, minute: minuteMark})
